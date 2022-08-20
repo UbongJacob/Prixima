26eb9643c916d822474ddd891b78f9975ee95ea9
Ubong Jacob <jacobubong12@gmail.com>
2022-08-20 18:03:56 +0100

    Added the services and about us sections

---
+++ javascript/app.js
@@ -0,0 +1,41 @@
+$(document).ready(function () {
+  // HERO SLIDER
+  $('#hero-slider').owlCarousel({
+    loop: true,
+    margin: 0,
+    nav: true,
+    items: 1,
+    dots: false,
+    smartSpeed: 1000,
+    navText: ['PREV', 'NEXT'],
+    responsive: {
+      0: {
+        nav: false,
+      },
+      768: {
+        nav: true,
+      },
+    },
+  });
+});
+
+// PROJECT SLIDER
+$('#project-slider').owlCarousel({
+  loop: true,
+  margin: 0,
+  nav: false,
+  dots: false,
+  smartSpeed: 1000,
+  margin: 24,
+  responsive: {
+    0: {
+      nav: false,
+    },
+    768: {
+      nav: true,
+    },
+    1140: {
+      items: 2,
+    },
+  },
+});
